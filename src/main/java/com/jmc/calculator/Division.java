5207476812fa02281741f790b2b8656fb922eafc
JNMC0 <20243ds126@utez.edu.mx>
2025-07-04 13:38:49 -0600

    ya acabe uwu

--- src/main/java/com/jmc/calculator/Division.java
+++ src/main/java/com/jmc/calculator/Division.java
@@ -24,8 +24,7 @@ protected void Division(){
     Double num1 = Double.parseDouble(txt1.getText());
     Double num2 = Double.parseDouble(txt2.getText());
     Double resultado = num1/num2;
-    lblResultado.setText(resultado.toString());
-
+    lblResultado.setText("EL resultado es: " + resultado);
     }
 
 }
